6a8e83d985c5e1a4fb41ecf95d02682052ea044b
Wojciech Kwolek <me@irth.pl>
2021-03-07 13:36:03 +0100

    category home screen

--- category_home.go
+++ category_home.go
@@ -26,6 +26,24 @@ func (c *CategoryHomeScreen) Scene() *simple.Scene {
 				simple.Pos(simple.Abs(100), simple.Abs(300), simple.Percent(100), simple.Abs(100)),
 				c.Category.Name,
 			),
+			simple.FontSize(48),
+			simple.NewLabel(
+				simple.Pos(simple.Abs(150), simple.Step, simple.Percent(100), simple.Abs(100)),
+				"Search",
+			),
+			simple.NewTextInput(
+				"search",
+				simple.Pos(simple.Step, simple.Same, simple.Percent(60), simple.Abs(50)),
+				"",
+				nil,
+			),
+			simple.FontSize(32),
+			simple.NewButton(
+				"search",
+				simple.Pos(simple.Abs(150), simple.Abs(500), simple.Percent(100), simple.Abs(50)),
+				"(back)",
+				nil,
+			),
 		},
 	}
 }
