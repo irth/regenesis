10ce3ba8825d07457cb8f99a7f27036f0b9b2fb0
Wojciech Kwolek <me@irth.pl>
2021-03-07 12:38:30 +0100

    Implement category selection

--- main.go
+++ main.go
@@ -2,61 +2,71 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
+	libgen "github.com/irth/golibgen"
 	"github.com/irth/regenesis/pkg/simple"
 )
 
+func Header() simple.Widget {
+	return simple.WidgetList{
+		simple.Left,
+		simple.FontSize(100),
+		simple.NewLabel(
+			simple.Pos(simple.Abs(100), simple.Abs(100), simple.Percent(100), simple.Abs(50)),
+			"LibGen",
+		),
+		simple.FontSize(32),
+	}
+}
+
 func main() {
-	butt := &simple.Button{
-		Position: simple.Position{
-			X:      simple.Absolute(10),
-			Y:      simple.Absolute(10),
-			Width:  simple.Percent(30),
-			Height: simple.Absolute(40),
-		},
-		ID:   "mybutton",
-		Name: "myname1",
-		OnClick: func(a *simple.App, b *simple.Button) error {
-			fmt.Println("button clicked!")
-			return nil
+	var selectedProvider libgen.SearchProvider = nil
+
+	clickHandler := func(a *simple.App, b *simple.Button) error {
+		switch b.ID {
+		case "category_libgen":
+			selectedProvider = &libgen.LibgenSearchProvider{}
+		case "category_fiction":
+			selectedProvider = &libgen.FictionSearchProvider{}
+		}
+		if selectedProvider != nil {
+			a.NextScene(&simple.Scene{
+				[]simple.Widget{
+					Header(),
+					simple.NewLabel(
+						simple.Pos(simple.Abs(100), simple.Abs(300), simple.Percent(100), simple.Abs(100)),
+						fmt.Sprintf("selected: %s", b.Name),
+					),
+				},
+			})
+		}
+		return nil
+	}
+
+	categorySelection := simple.Scene{
+		Widgets: []simple.Widget{
+			Header(),
+			simple.NewLabel(
+				simple.Pos(simple.Abs(100), simple.Abs(300), simple.Percent(100), simple.Abs(100)),
+				"Choose a category",
+			),
+			simple.FontSize(64),
+			simple.NewButton(
+				"category_libgen",
+				simple.Pos(simple.Abs(100), simple.Step, simple.Percent(100), simple.Abs(100)),
+				"Sci-Tech",
+				clickHandler,
+			),
+			simple.NewButton(
+				"category_fiction",
+				simple.Pos(simple.Abs(100), simple.Step, simple.Percent(100), simple.Abs(100)),
+				"Fiction",
+				clickHandler,
+			),
 		},
 	}
 
-	butt2 := &simple.Button{
-		Position: simple.Position{
-			X:      simple.Same,
-			Y:      simple.Step,
-			Width:  simple.Percent(30),
-			Height: simple.Absolute(40),
-		},
-		ID:   "mybutton2",
-		Name: "myname2",
-		OnClick: func(a *simple.App, b *simple.Button) error {
-			fmt.Println("second button clicked!")
-			return nil
-		},
-	}
-
-	input := &simple.TextInput{
-		Position: simple.Position{
-			X:      simple.Same,
-			Y:      simple.Step,
-			Width:  simple.Percent(30),
-			Height: simple.Absolute(40),
-		},
-		ID: "textinput",
-		OnUpdate: func(a *simple.App, t *simple.TextInput) error {
-			t.Value = strings.ToTitle(t.Value)
-			return nil
-		},
-	}
-
-	scene := simple.Scene{
-		Widgets: []simple.Widget{butt, butt2, input},
-	}
-
-	app := simple.NewApp(&scene)
+	app := simple.NewApp(&categorySelection)
 
 	err := app.RunForever()
 	if err != nil {
